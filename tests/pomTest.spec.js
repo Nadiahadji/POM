5e5bb836c0191c3c73385f5bb90aabcd668a78b9
Nada <100967273+Nadiahadji@users.noreply.github.com>
2026-01-04 00:33:18 +0100

    Update pomTest.spec.js

--- tests/pomTest.spec.js
+++ tests/pomTest.spec.js
@@ -1,34 +1,34 @@
 import {test,expect} from '@playwright/test'
 import PomManager from '../pages/PomManager';
 let pm;
-// test.describe('Test Login',()=>{
-//     test.beforeEach(async ({page})=>{
-//         pm =new PomManager(page);
-//             await page.pause();
-//     })
-//     test.afterEach(async ({page})=>{
-//         await page.close();
-//     })
-//     test('test1,login with valid credentials',async()=>{
+test.describe('Test Login',()=>{
+    test.beforeEach(async ({page})=>{
+        pm =new PomManager(page);
+            await page.pause();
+    })
+    test.afterEach(async ({page})=>{
+        await page.close();
+    })
+    test('test1,login with valid credentials',async()=>{
     
-//         await pm.LoginPage.navigate();
-//         await pm.LoginPage.login('tomsmith','SuperSecretPassword!');
-//         await pm.SecurePage.assertLoggedInMesage('You logged into a secure area!');
+        await pm.LoginPage.navigate();
+        await pm.LoginPage.login('tomsmith','SuperSecretPassword!');
+        await pm.SecurePage.assertLoggedInMesage('You logged into a secure area!');
 
 
-//         // assert value in direct test 
-//         const msg = await pm.SecurePage.getMessage();
-//         expect(msg).toContain('You logged into a secure area!');
+        // assert value in direct test 
+        const msg = await pm.SecurePage.getMessage();
+        expect(msg).toContain('You logged into a secure area!');
 
 
-//     })
-//     test.only('test2,login with invalid credentials',async()=>{
-//         await pm.LoginPage.navigate();
-//         await pm.LoginPage.login('test','testttt');
-//         await pm.LoginPage.assertErrorMesaage('Your username is invalid!');
-//     })
+    })
+    test.only('test2,login with invalid credentials',async()=>{
+        await pm.LoginPage.navigate();
+        await pm.LoginPage.login('test','testttt');
+        await pm.LoginPage.assertErrorMesaage('Your username is invalid!');
+    })
 
-// })
+})
 
 test.describe.only('test des checkbox',()=>{
         test.beforeEach(async ({page})=>{
